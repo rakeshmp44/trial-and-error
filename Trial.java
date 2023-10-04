adfe74be5a3a9961316058e8748db54a653e17aa
rakeshmp44 <rakeshmp004@gmail.com>
2023-10-04 16:20:04 +0530

    added subtratcion feature

--- Trial.java
+++ Trial.java
@@ -2,5 +2,6 @@ class Trial{
     public static void main(String[] args) {
         int a=10,b=10;
         System.out.println("sum is: "+(a+b));
+        System.out.println("diff is: "+(a-b));
     }
 }
